efd3e105bda2ade3c37a69c309f273b8b0880851
Frederick Tetteh <kwesiossom@gmail.com>
2022-07-20 22:19:46 -0400

    Day 8 done

---
+++ Day8/Caesar_Cipher.py
@@ -0,0 +1,65 @@
+from Art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+print(logo)
+
+
+def split(word):
+    return list(word)
+
+
+def encrypt(txt, shft):
+    textArr = split(txt)
+
+    for i in range(len(textArr)):
+        ltr = textArr[i]
+        if ltr != ' ':
+            indx = alphabet.index(ltr)
+            newIndx = indx + shft
+            if newIndx >= len(alphabet):
+                newIndx %= 26
+
+            textArr[i] = alphabet[newIndx]
+
+    return "".join(textArr)
+
+
+def decrypt(txt, shft):
+    textArr = split(txt)
+
+    for i in range(len(textArr)):
+        ltr = textArr[i]
+        if ltr != ' ':
+            indx = alphabet.index(ltr)
+            newIndx = indx - shft
+            if newIndx < 0:
+                newIndx += 26
+
+            textArr[i] = alphabet[newIndx]
+
+    return "".join(textArr)
+
+
+def caesar(dir, txt, shft):
+    results = ""
+    if dir == 'encode':
+        results = encrypt(txt, shft)
+    elif dir == 'decode':
+        results = decrypt(txt, shft)
+
+    print(f"The {direction}d value is {results}")
+
+
+switch = True;
+while switch:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(direction, text, shift)
+
+    choice = input("Type 'yes' to rerun, type 'no' to stop:\n")
+    if choice == 'yes':
+        switch = True
+    else:
+        switch = False
