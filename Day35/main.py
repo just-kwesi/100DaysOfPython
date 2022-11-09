58461b12bbff5025385e00c8e236f854eff915bb
Frederick Tetteh <kwesiossom@gmail.com>
2022-11-09 10:27:12 -0500

    day35 done

--- Day35/main.py
+++ Day35/main.py
@@ -1,16 +1,43 @@
-import requests
 from dotenv import dotenv_values
-from datetime import datetime
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
 
 config = dotenv_values(".env")
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+account_sid = "YOUR ACCOUNT SID"
+auth_token = os.environ.get("AUTH_TOKEN")
 
-parameters = {
+weather_params = {
     "appid": config["APPID"],
     "lat": 40.664943,
     "lon": -73.882736,
     "units": "imperial"
 }
 
-response = requests.get(url="https://api.openweathermap.org/data/2.5/weather", params=parameters)
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
 
-print(response.status_code)
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
+    )
+    print(message.status)
