385702e6c41f7d5fba67aa8e9d7e4f6252b72909
Frederick Tetteh <kwesiossom@gmail.com>
2023-01-01 21:38:22 -0500

    day 40

--- Day39and40/main.py
+++ Day39and40/main.py
@@ -21,10 +21,13 @@ sheet_data = data.getFlight().flights
 # confirm all rows include IATA codes
 for flight_row in sheet_data:
     if flight_row["iataCode"] == "":
-        iataCode = flight_srch.getIataCode(flight_row["city"])  # get IATA code from kiwi.api
+        iataCode = flight_srch.getIataCode(
+            flight_row["city"])  # get IATA code from kiwi.api
         flight_id = flight_row["id"]  # get id for update of row
-        flight_row["iataCode"] = iataCode[0]["code"]  # update row to include IATA code
-        status = data.updateFlight(flight_id, flight_row)  # send a put request to update hoogle sheet
+        # update row to include IATA code
+        flight_row["iataCode"] = iataCode[0]["code"]
+        # send a put request to update hoogle sheet
+        status = data.updateFlight(flight_id, flight_row)
 
 # iterate for flight prices and send sms if lesser than sheets prices
 
@@ -40,4 +43,5 @@ for loc in sheet_data:
     if flights and cheap_flights:
         send_notification.sendMessages(flight_price.flights)
         emails = data.getUsers().users
-        send_notification.sendEmails(emails=emails, flight=flight_price.flights[0])
+        send_notification.sendEmails(
+            emails=emails, flight=flight_price.flights[0])
